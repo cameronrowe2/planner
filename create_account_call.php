beb0ccf9de0b2d7f6f334f87184756c72f68ad64
Cameron Rowe <cameronroweau@gmail.com>
2018-12-10 18:59:44 +1000

    Added CreateAccountAttempts to site.

--- create_account_call.php
+++ create_account_call.php
@@ -10,7 +10,30 @@ $password2 = $_GET['password2'];
 
 $mysqli = m_connect();
 
-// check if email exists
+
+$date = date('Y-m-d');
+$addr = $_SERVER['REMOTE_ADDR'];
+
+$stmt = $mysqli->prepare("SELECT ID FROM CreateAccountAttempts where date = ? and addr = ?");
+$stmt->bind_param("ss", $date, $addr);
+
+if( !$stmt->execute() ) {
+    echo json_encode(["success" => false]);
+    die();
+}
+
+$stmt->bind_result($ID_res);
+
+$count = 0;
+while($stmt->fetch()){
+    $count++;
+}
+
+if($count > 2) {
+    echo json_encode(["success" => false]);
+    exit();
+}
+
 
 
 $stmt = $mysqli->prepare("SELECT ID FROM Login where email like ?");
@@ -38,6 +61,19 @@ if($count > 0) {
 if($password != $password2) {
     echo json_encode(["success" => false]);
     exit();
+}
+
+// store CreateAccountAttempt in database
+$stmt2 = $mysqli->prepare("INSERT INTO CreateAccountAttempts (date, addr) VALUES (?, ?)");
+
+if (!($stmt2)) {
+    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+}
+$stmt2->bind_param("ss", $date, $addr);
+
+if( !$stmt2->execute() ) {
+    echo json_encode(["success" => false]);
+    die();
 }
 
 // hash password
