bdf3c5eddd51d21e9fb54c47f30083809d178c04
cameronrowe2 <cameronroweau@gmail.com>
2020-10-30 21:58:08 +1000

    Update db.php
    updated db.php

--- db.php
+++ db.php
@@ -6,12 +6,6 @@ define("USERNAME", "root");
 define("PASSWORD", "root");
 define("DBNAME", "planner");
 
-// Live
-// define("HOST", "localhost");
-// define("USERNAME", "planner6_root");
-// define("PASSWORD", "JU1CYl3m0n");
-// define("DBNAME", "planner6_planner");
-
 // echo CONSTANT; // outputs "Hello world."
 
 function m_connect() {
